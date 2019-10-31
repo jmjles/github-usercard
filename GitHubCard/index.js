15997a57f25cb6950ecbbc33869ecd3658594015
jmjles <jmjles@gmail.com>
2019-10-31 14:18:55 -0700

    stretch complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -18,20 +18,35 @@ let cardConstruct = (user) => {
   let cards = document.querySelector('.cards');
   let template =
   `
+  <a href=${user.html_url} target="_blank">
   <div class="card">
   <img src=${user.avatar_url}/>
   <div class="card-info">
     <h3 class="name"> ${user.name}</h3>
     <p class="username"> ${user.login}</p>
     <p>Location: ${user.location}</p>
-    <p>Profile:  
-      <a href={address to users github page}> ${user.url}</a>
-    </p>
     <p>Followers: ${user.followers}</p>
     <p>Following: ${user.following}</p>
     <p>Bio: ${user.bio}</p>
   </div>
 </div>
-  `
-  cards.innerHTML += template
+</a>
+  `;
+  cards.innerHTML += template;
+  const card = document.querySelectorAll('.card');
+
+  card.forEach(item =>{
+    item.addEventListener('mouseenter',()=>
+      anime({
+        targets:item,
+        scale:1.2
+      })
+  )
+    item.addEventListener('mouseleave', () =>
+      anime({
+        targets: item,
+        scale: 1
+      })
+    )
+})
 }
